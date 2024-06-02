7eb6057c509e14e6d584827273aa4e73acb76414
Olumuyiwa Taiwo <muyi.taiwo@logicexpertise.com>
2024-06-02 14:38:26 +1200

    local-with-hl-bugfixes

---
+++ classes/form/topic_item_select_form.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * OJT topic item select form
+ *
+ * @package   mod_ojt
+ * @author    Alex Morris <alex.morris@catalyst.net.nz
+ * @copyright Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_ojt\form;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * OJT topic item select form
+ *
+ * @package   mod_ojt
+ * @author    Alex Morris <alex.morris@catalyst.net.nz
+ * @copyright Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class topic_item_select_form extends topic_item_form {
+    function definition() {
+        parent::definition();
+        $mform =& $this->_form;
+
+        $mform->addElement('hidden', 'type');
+        $mform->setType('type', PARAM_INT);
+        $mform->setDefault('type', OJT_ITEM_TYPE_SELECT);
+
+        $mform->addelement(
+            'textarea',
+            'selectionoptions',
+            get_string('selectionoptions', 'ojt'),
+            array('rows' => 8, 'cols' => 40)
+        );
+        $mform->setType('selectionoptions', PARAM_TEXT);
+        $mform->setDefault('selectionoptions', get_config('mod_ojt', 'topicitemselectdefaults'));
+        $mform->addHelpButton('selectionoptions', 'selectionoptions', 'ojt');
+
+        $this->add_action_buttons(false);
+    }
+}
